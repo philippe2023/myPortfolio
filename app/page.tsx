d093fe7d3ee2d5995c56cc9f263be623cd4d6092
Jean Philippe Auguste <123325152+philippe2023@users.noreply.github.com>
2024-07-09 15:31:22 +0200

    work on the bento box design and add the nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav 
+        navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />},
+          {name: 'About', link: '/', icon: <FaHome />},
+          {name: 'Project', link: '/', icon: <FaHome />},
+          {name: 'Resume', link: '/', icon: <FaHome />},
+          {name: 'Blog', link: '/', icon: <FaHome />},
+          {name: 'Contact', link: '/', icon: <FaHome />}
+        ]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <section id='hero' className="pb-20 pt-36">
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
@@ -36,7 +36,7 @@ const Hero = () => {
                 
             </div>
         </div>
-    </div>
+    </section>
   )
 }
 
